b4d25cd12ebcbf911538554687fc01771c74afb3
hdpham22 <103155924+hdpham22@users.noreply.github.com>
2023-05-06 13:48:22 -0700

    Expose and some of explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,42 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  //create necessary variables
+  const synth = window.speechSynthesis;
+  const voiceSelection = document.getElementById('voice-select');
+
+  const speakButton = document.querySelector('button');
+  let toSpeak = document.getElementById('text-to-speak');
+
+  let voices;
+
+  const face = document.querySelector('img');
+
+  function loadVoices() {
+    voices = synth.getVoices();
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+      option.value = i;
+      voiceSelection.appendChild(option);
+    }
+  }
+
+  if ("onvoiceschanged" in synth) {
+    synth.onvoiceschanged = loadVoices;
+  } else {
+    loadVoices();
+  }
+  
+
+  speakButton.addEventListener('click', () => {
+    let utterance = new SpeechSynthesisUtterance(toSpeak.value);
+    utterance.voice = voices[voiceSelection.value];
+  });
+
+  synth.speak(utterance);
+
+
+  
 }
+
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,50 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const hornSelect = document.getElementById('horn-select');
+  const hornImage = document.querySelector("img");
+
+  const playButton = document.querySelector('button');
+  const jsConfetti = new JSConfetti();
+  const hornSound = document.querySelector('audio');
+  
+  const audioControls = document.getElementById('volume');
+  let audioPath = '';
+
+  const volImage = document.querySelector('#volume-controls img');
+
+  //event listener for changing the image of the horn
+  hornSelect.addEventListener("change", (event) => {
+    const imgPath = 'assets/images/' + event.target.value + '.svg';
+
+    audioPath = 'assets/audio/' + event.target.value + '.mp3'
+    hornSound.src = audioPath;
+
+    hornImage.src = imgPath;
+  });
+
+  //event listener for clicking the play button to play sound
+  playButton.addEventListener("click", () => {
+    hornSound.volume = audioControls.value / 100;
+    hornSound.play();
+
+    if(audioPath == 'assets/audio/party-horn.mp3') {
+      jsConfetti.addConfetti();
+    }
+  });
+  
+  //event listener for changing the volume of the horn
+  audioControls.addEventListener("input", () => {
+
+    if(audioControls.value >= 1 && audioControls.value < 33) {
+      volImage.src = 'assets/icons/volume-level-1.svg';
+    } else if(audioControls.value >= 33 && audioControls.value < 67) {
+      volImage.src = 'assets/icons/volume-level-2.svg';
+    } else if (audioControls.value >= 67) {
+      volImage.src = 'assets/icons/volume-level-3.svg';
+    } else {
+      volImage.src = 'assets/icons/volume-level-0.svg';
+    }
+
+  });
 }
